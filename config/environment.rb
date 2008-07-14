e4b5a7a0f67746757c645f777bf4eb894e3864bb
Adam Hooper <adam@adamhooper.com>
2008-07-14 14:56:20 -0400

    Upgrade haml

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,7 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', :version => '2.3.2', :source => 'http://gems.github.com/', :lib => 'will_paginate'
   config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   config.gem 'rmagick', :lib => 'RMagick' # for captcha
-  config.gem 'haml', :version => '2.0.0'
+  config.gem 'haml', :version => '2.0.1'
   config.gem 'RedCloth', :source => 'http://code.whytheluckystiff.net', :version => '3.301', :lib => 'redcloth'
   config.gem 'ultraviolet', :lib => 'uv'
   config.gem 'mime-types', :lib => 'mime/types'
