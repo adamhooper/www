298751229ac17310deb522c11ec288fa2ca71d9a
Adam Hooper <adam@adamhooper.com>
2008-07-29 23:44:28 -0400

    Redirect old blog to new one

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.simple_captcha '/simple_captcha/:action', :controller => 'simple_captcha'
 
+  map.connect '/blog/index.rss', :controller => 'Blog::Posts', :format => 'rss'
+
   map.blog '/blog', :controller => 'blog/posts'
 
   # Install the default routes as the lowest priority.
