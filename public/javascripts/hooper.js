99c717af93172d5952e8d599c13bb2076b1fef34
Adam Hooper <adam@adamhooper.com>
2010-04-16 08:36:58 +0300

    Hide Tags until the user hovers (or clicks)

--- public/javascripts/hooper.js
+++ public/javascripts/hooper.js
@@ -104,4 +104,32 @@ var flexiBackground = function(){
 	};
 }();
 
+$(function() {
+	var $tags = $('#tags');
+	if (!$tags.length) return;
+
+	var $h3 = $tags.children('h3');
+	var $ul = $tags.children('ul');
+
+	var amount_to_hide = $ul.height();
+
+	$tags.css('bottom', -amount_to_hide);
+
+	$(window).load(function() {
+		// Dunno why but Chrome and Firefox are wrong on "ready" event
+		amount_to_hide = $ul.height();
+		$tags.css('bottom', -amount_to_hide);
+	});
+
+	$tags.hover(function() {
+		$tags.animate({bottom: 0}, {queue: false});
+	}, function() {
+		$tags.animate({bottom: -amount_to_hide}, {queue: false});
+	});
+	$tags.click(function() {
+		amount_to_hide = 0;
+		$tags.animate({bottom: 0});
+	});
+});
+
 $(document).ready(flexiBackground.initialize);
