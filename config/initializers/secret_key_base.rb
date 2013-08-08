c4212fb8d820ad1cc6d4b546619a567e862c3502
Adam Hooper <adam@adamhooper.com>
2013-08-08 13:55:45 -0400

    Use better variable name in initializers

--- config/initializers/secret_key_base.rb
+++ config/initializers/secret_key_base.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_key_base = '6777dd43fdad5949861f19140e659b3cdedc12b5b3eee7a982b2a2a7e76a9acd9e00ef4eede46057144a833effd2d59a416c3e9d35becaeb3fdc49d374cb0a57'
+Www::Application.config.secret_key_base = '6777dd43fdad5949861f19140e659b3cdedc12b5b3eee7a982b2a2a7e76a9acd9e00ef4eede46057144a833effd2d59a416c3e9d35becaeb3fdc49d374cb0a57'
