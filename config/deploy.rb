b2bd875c96ddca666c8c22552cdf6965c97cb98e
Adam Hooper <adam@adamhooper.com>
2013-08-08 13:06:44 -0400

    Upgrade to Capistrano 3, pushing to Amazon

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,55 +1,18 @@
-require 'bundler/capistrano'
-load 'deploy/assets'
+set :application, 'www'
+set :repo_url, 'file:///home/git/www.git'
 
-set :application, "www"
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
+set :deploy_to, '/opt/rails/www'
 set :scm, :git
-set :git_enable_submodules, true
-
-set :user, 'ubuntu'
-set :use_sudo, false
 set :branch, 'master'
-set :repository,  "ssh://git@git.adamhooper.com/home/git/www.git"
-set :deploy_via, :remote_cache
-set :deploy_to, "/opt/rails/#{application}"
+
+set :format, :pretty
+set :log_level, :debug
+# set :pty, true
+
+set :linked_files, %w{db/production.sqlite3 config/environments/production.rb config/admin-password}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
-
-role :app, "amazon"
-role :web, "amazon"
-role :db, "amazon", :primary => true
-
-ssh_options[:keys] = "#{ENV['home']}/.ssh/adamhooper-on-amazon-id_rsa"
-ssh_options[:forward_agent] = true
-
-default_run_options[:pty] = true
-
-namespace :deploy do
-  desc "Does not start the server (we use Passenger)"
-  task :start, :roles => :app do
-  end
-
-  desc "Does not stop the server (we use Passenger)"
-  task :stop, :roles => :app do
-  end
-
-  desc "Restarts the server (touching a file -- we use Passenger)"
-  task :restart, :roles => :app do
-    run %(touch #{latest_release}/tmp/restart.txt)
-  end
-
-  desc "Fixes permissions and updates symlinks"
-  task :after_update_code, :roles => :app do
-    sudo %(chown www-data:www-data #{latest_release}/config/environment.rb)
-    run "ln -nfs #{shared_path}/config/admin-password #{release_path}/config/admin-password" 
-  end
-end
-
-after "deploy:update_code", "db:symlink" 
-
-namespace :db do
-  desc "Create symlink to the real database" 
-  task :symlink do
-    run "touch #{shared_path}/database/production.sqlite3" # if it doesn't exist, create empty database
-    run "ln -nfs #{shared_path}/database/production.sqlite3 #{release_path}/database/production.sqlite3"
-  end
-end
