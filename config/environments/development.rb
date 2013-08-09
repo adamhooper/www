82e0044fea968f5588b0065166a933a0d82fdd9d
Adam Hooper <adam@adamhooper.com>
2013-08-09 16:46:07 -0400

    Remove another deprecated option

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,8 +7,6 @@ Www::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'localhost', :port => 3000 }
 
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
-
   config.assets.compress = false
   config.assets.debug = true
 
